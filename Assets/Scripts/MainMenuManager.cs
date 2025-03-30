01fa6c39ec63145781428d4fcd8b6977529a4100
Gluckasz <lukas.gluck.04@gmail.com>
2025-03-30 18:57:37 +0200

    Added showing options menu and mixers

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,15 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.XR.Interaction.Toolkit.UI;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public static GameObject mainMenuInstance;
+    public static GameObject optionsMenuInstance;
+
     public GameObject xROrigin;
+
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+
     void Start()
     {
         LocomotionDisabler locomotionDisablerScript = xROrigin.GetComponent<LocomotionDisabler>();
-
         locomotionDisablerScript.enableMovement = false;
         locomotionDisablerScript.enableTurning = false;
+
+        ShowMainMenu();
     }
 
     public void StartGame()
@@ -22,7 +31,49 @@ public class MainMenuManager : MonoBehaviour
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
-            Application.Quit();
+        Application.Quit();
 #endif
+    }
+
+    public void ShowMainMenu()
+    {
+        if (mainMenuInstance == null)
+        {
+            mainMenuInstance = Instantiate(mainMenu);
+            if (mainMenuInstance != null)
+            {
+                mainMenuInstance.SetActive(true);
+            }
+        }
+        else
+        {
+            mainMenuInstance.SetActive(true);
+        }
+
+        if (optionsMenuInstance != null)
+        {
+            optionsMenuInstance.SetActive(false);
+        }
+    }
+
+    public void ShowOptionsMenu()
+    {
+        if (optionsMenuInstance == null)
+        {
+            optionsMenuInstance = Instantiate(optionsMenu);
+            if (optionsMenuInstance != null)
+            {
+                optionsMenuInstance.SetActive(true);
+            }
+        }
+        else
+        {
+            optionsMenuInstance.SetActive(true);
+        }
+
+        if (mainMenuInstance != null)
+        {
+            mainMenuInstance.SetActive(false);
+        }
     }
 }
