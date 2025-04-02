2ff2f0acdb8d211863bc2e3f095146cb8c983ef2
Gluckasz <lukas.gluck.04@gmail.com>
2025-04-02 15:43:07 +0200

    Added functionality to read JSON

---
+++ Assets/Scripts/DialogueClasses.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class DialogueData
+{
+    public List<DialogueNode> intro;
+    public List<DialogueNode> scene;
+}
+
+[Serializable]
+public class DialogueNode
+{
+    public string id;
+    public string character;
+    public string text;
+    public string audio;
+    public List<DialogueChoice> choices;
+}
+
+[Serializable]
+public class DialogueChoice
+{
+    public string shortText;
+    public string text;
+    public string nextId;
+    public string item;
+}
--- Assets/Scripts/SceneFlowManager.cs
+++ Assets/Scripts/SceneFlowManager.cs
@@ -1,16 +1,34 @@
+using System.IO;
 using UnityEngine;
 
 public class SceneFlowManager : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public DialogueData dialogueData;
+
     void Start()
     {
-        
+        LoadDialogueData();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LoadDialogueData()
     {
-        
+        string filePath = Path.Combine(Application.dataPath, "scene_1_script.json");
+
+        if (File.Exists(filePath))
+        {
+            string jsonContent = File.ReadAllText(filePath);
+
+            dialogueData = JsonUtility.FromJson<DialogueData>(jsonContent);
+
+            Debug.Log("Dialogue loaded successfully!");
+            if (dialogueData.intro.Count > 0)
+            {
+                Debug.Log("First node text: " + dialogueData.intro[0].text);
+            }
+        }
+        else
+        {
+            Debug.LogError("Could not find JSON file at path: " + filePath);
+        }
     }
 }
