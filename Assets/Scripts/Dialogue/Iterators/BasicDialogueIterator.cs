9ffdc7435d566e17ae6312e1ded2515bc7eef3d0
Gluckasz <lukas.gluck.04@gmail.com>
2025-05-15 20:19:30 +0200

    Renamed variables for better clarity

--- Assets/Scripts/Dialogue/Iterators/BasicDialogueIterator.cs
+++ Assets/Scripts/Dialogue/Iterators/BasicDialogueIterator.cs
@@ -3,19 +3,19 @@ using UnityEngine;
 
 public class BasicDialogueIterator : DialogueIterator
 {
-    private readonly DialogueCollection intro_;
+    private readonly DialogueCollection dialogue_;
     private string currentId_;
 
-    public BasicDialogueIterator(DialogueCollection intro)
+    public BasicDialogueIterator(DialogueCollection dialogue)
     {
-        intro_ = intro;
+        dialogue_ = dialogue;
     }
 
     public DialogueNode GetNode()
     {
         if (HasMore(currentId_))
         {
-            DialogueNode result = intro_.dialogueNodes[currentId_];
+            DialogueNode result = dialogue_.dialogueNodes[currentId_];
             currentId_ = result.nextId;
             return result;
         }
@@ -27,7 +27,7 @@ public class BasicDialogueIterator : DialogueIterator
 
     public bool HasMore(string id)
     {
-        return intro_.dialogueNodes.ContainsKey(id);
+        return dialogue_.dialogueNodes.ContainsKey(id);
     }
 
     public void SetId(string newId)
