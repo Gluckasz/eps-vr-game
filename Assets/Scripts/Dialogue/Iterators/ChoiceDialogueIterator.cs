b8a9bdfc7399cc9dc6a5b83b9b23a53a1ab9a2bf
Gluckasz <lukas.gluck.04@gmail.com>
2025-05-15 20:28:40 +0200

    Added collection and iterator for choice dialogue

---
+++ Assets/Scripts/Dialogue/Iterators/ChoiceDialogueIterator.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class ChoiceDialogueIterator : DialogueIterator
+{
+    private readonly DialogueCollection dialogue_;
+    private string currentId_;
+
+    public ChoiceDialogueIterator(DialogueCollection dialogue)
+    {
+        dialogue_ = dialogue;
+    }
+
+    public DialogueNode GetNode()
+    {
+        if (HasMore(currentId_))
+        {
+            DialogueNode result = dialogue_.dialogueNodes[currentId_];
+            return result;
+        }
+        else
+        {
+            return null;
+        }
+    }
+
+    public bool HasMore(string id)
+    {
+        return dialogue_.dialogueNodes.ContainsKey(id);
+    }
+
+    public void SetId(string newId)
+    {
+        currentId_ = newId;
+    }
+}
