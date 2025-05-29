c42c19b496623d0b02df03698cb42093d6a559cd
Gluckasz <lukas.gluck.04@gmail.com>
2025-05-29 18:30:23 +0200

    Design changes to feedback window placement

--- Assets/Scripts/Dialogue/UI/ChoiceDialogueDisplay.cs
+++ Assets/Scripts/Dialogue/UI/ChoiceDialogueDisplay.cs
@@ -24,7 +24,7 @@ public class ChoiceDialogueDisplay : MonoBehaviour, DialogueDisplay
         { "Mother", new(0.5f, 1.3f, -0.2f) },
         { "Sibling", new(0.3f, 1.3f, 0.6f) },
         { "Narrator", new(-3.94f, 1.2f, -1f) },
-        { "Feedback", new(0, 0, -0.5f) },
+        { "Feedback", new(0, 0, 0.3f) },
     };
     private readonly List<string> characterTags = new()
     {
